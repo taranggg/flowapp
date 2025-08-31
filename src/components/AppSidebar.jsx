8d7cd8fc1b128bd6e3366e4c6ce8275d69a5ca46
Tarang Kumar <139793828+taranggg@users.noreply.github.com>
2025-08-31 20:15:35 +0530

    feat: implement sidebar navigation with AppSidebar component

---
+++ src/components/AppSidebar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+import { Waypoints, Workflow } from "lucide-react";
+
+const items = [
+  {
+    title: "Chatflow",
+    url: "/chatflow",
+    icon: Waypoints,
+  },
+  {
+    title: "Agentflow",
+    url: "/agentflow",
+    icon: Workflow,
+  },
+];
+
+function AppSidebar() {
+  return (
+    <Sidebar variant="floating" className={"top-20"}>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
+
+export default AppSidebar;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import Navbar from "@/components/navbar";
+import { SidebarProvider, SidebarTrigger } from "../components/ui/sidebar";
+import AppSidebar from "../components/AppSidebar";
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-b from-background to-red-100 dark:bg-gradient-to-b dark:from-black dark:to-red-100">
-      <Navbar />
-      <main className="flex-1 flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-center">Welcome to FlowApp</h1>
-        <p className="mt-4 text-lg text-center">
-          Your one-stop solution for all your workflow needs.
-        </p>
-      </main>
-    </div>
+    <SidebarProvider defaultOpen={true}>
+      <div className="flex flex-col min-h-screen w-full">
+        <Navbar />
+        <div>
+          <AppSidebar />
+          <main className="flex-1 flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-center">
+              Welcome to FlowApp
+            </h1>
+            <p className="mt-4 text-lg text-center">
+              Your one-stop solution for all your workflow needs.
+            </p>
+          </main>
+        </div>
+      </div>
+    </SidebarProvider>
   );
 }
 
